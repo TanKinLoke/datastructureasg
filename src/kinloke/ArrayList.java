703aad00ac9fc95518812ec17aaf6ec3536f457c
unknown <kinloketan@gmail.com>
2022-09-04 11:03:22 +0800

    Removed another auto generated comment

--- src/kinloke/ArrayList.java
+++ src/kinloke/ArrayList.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kinloke;
 
-/**
- *
- * @author user
- */
 public class ArrayList<T> implements ListInterface<T> {
     private T[] array;
     private int number;
